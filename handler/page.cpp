c61d2dcc97e88b4c9ad1fb0e621862ec5db4dbad
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-01 22:29:50 +0800

    printout more

--- handler/page.cpp
+++ handler/page.cpp
@@ -21,7 +21,7 @@ namespace handler
     void not_found_page(cinatra::request &req, cinatra::response &res){
         SPDLOG_INFO("404 not found page, ip={}", req.get_header_value("X-Forwarded-For"));
 
-        res.set_status_and_content(status_type::ok, utils::read_template("template/html/404_not_found.html"), req_content_type::html, content_encoding::gzip);
+        res.set_status_and_content(status_type::not_found, utils::read_template("template/html/404_not_found.html"), req_content_type::html, content_encoding::gzip);
     }
 
 
