965c4f125b29c047236ab115a4266c04620012d5
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-02 10:31:39 +0800

    remove some log

--- handler/handler.cpp
+++ handler/handler.cpp
@@ -13,14 +13,6 @@ namespace handler
 
     void upload_file(cinatra::request &req, cinatra::response &res)
     {
-        SPDLOG_INFO("head={}", req.head());
-        if(req.body().empty()){
-            SPDLOG_INFO("body is empty");
-        }
-        for(auto i=0; req.body().data()[i] != '\0';i++){
-            SPDLOG_INFO("i={}",i);
-        }
-        SPDLOG_INFO("body_length()={}, body.length()={}", req.body_len(),req.body().length());
         auto file = req.get_file();
         if(file==nullptr){
             res.set_status_and_content(status_type::bad_request,"no file provided", req_content_type::string);
