cb3498d67f6d50f6907bfdd2d11caf5ae73ab99f
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-03 22:20:24 +0800

    add

--- db/db_manager.cpp
+++ db/db_manager.cpp
@@ -7,20 +7,20 @@ namespace db
     }
 
     //Initialize the connection, if failed, the program should crash
-    thread_local pqxx::connection Database::conn = []() noexcept{
-        auto psql_str = std::getenv("DATABASE_URL");
-        if(psql_str==nullptr){
-            SPDLOG_ERROR("environment variable DATABASE_URL needed");
-            std::terminate();
-        }
-        pqxx::connection conn{psql_str};
-        if(!conn.is_open()){
-            SPDLOG_ERROR("connection does not open");
-            std::terminate();
-        }
-        SPDLOG_INFO("connection has established, url={}", psql_str);
-        return conn;
-    }();
+    // thread_local pqxx::connection Database::conn = []() noexcept{
+    //     auto psql_str = std::getenv("DATABASE_URL");
+    //     if(psql_str==nullptr){
+    //         SPDLOG_ERROR("environment variable DATABASE_URL needed");
+    //         std::terminate();
+    //     }
+    //     pqxx::connection conn{psql_str};
+    //     if(!conn.is_open()){
+    //         SPDLOG_ERROR("connection does not open");
+    //         std::terminate();
+    //     }
+    //     SPDLOG_INFO("connection has established, url={}", psql_str);
+    //     return conn;
+    // }();
     
 } // namespace db
 
