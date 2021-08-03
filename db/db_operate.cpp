42b6dec02acdc7003b75ae104cdda03491e2732c
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-04 00:15:44 +0800

    try

--- db/db_operate.cpp
+++ db/db_operate.cpp
@@ -6,7 +6,7 @@ namespace db
     void database_init()
     {
         //create t_user_info
-        if(auto result = exec(R"sql(CREATE TABLE t_user_info (
+        if(auto result = exec(R"sql(CREATE TABLE IF NOT EXISTS t_user_info (
             id              SERIAL PRIMARY KEY,
             user_name       VARCHAR(200) NOT NULL,
             password        VARCHAR(200) NOT NULL,
@@ -15,7 +15,16 @@ namespace db
         ))sql"); !result){
             SPDLOG_ERROR("Error when check if table t_user_info exists");
         }
-        
+
+
+        //check
+        pqxx::work t(Database::get_conn());
+        auto result = t.exec(R"sql(SELECT table_schema,table_name FROM information_schema.tables;)sql");
+        for(auto it=result.begin();it!=result.end();it++){
+            for(auto cit = it.begin();cit != it.end();cit++){
+                SPDLOG_INFO("{}", cit.c_str());
+            }
+        }
 
     }
 
@@ -26,7 +35,6 @@ namespace db
         {
             pqxx::work t(Database::get_conn());
             auto result = t.exec(sql);
-            SPDLOG_INFO("result={}", result.size());
             return result.empty();
         }
         catch (std::exception &err)
