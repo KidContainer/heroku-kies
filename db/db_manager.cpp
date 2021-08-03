902a067235ad248835c6252c088b8e29557372ee
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-03 20:00:15 +0800

    change exception to terminate

--- db/db_manager.cpp
+++ db/db_manager.cpp
@@ -11,11 +11,11 @@ namespace db
         auto psql_str = std::getenv("DATABASE_URL");
         if(psql_str==nullptr){
             SPDLOG_ERROR("environment variable DATABASE_URL needed");
-            throw std::runtime_error("DATABASE_URL is missing");
+            std::terminate();
         }
         pqxx::connection conn(psql_str);
         if(!conn.is_open()){
-            throw std::runtime_error("connection is not established");
+            std::terminate();
         }
         return conn;
     }();
