b21d57ffe2d0e266594033c0b258793c0a313945
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-05 15:04:00 +0800

    fix compile bug

--- main.cpp
+++ main.cpp
@@ -18,31 +18,31 @@ void bind_page(http_server &server)
 void bind_api(http_server &server)
 {
     server.set_http_handler<POST>("/api/upload_file", handler::upload_file); //Update file handler
-    server.set_http_handler<GET>("/api/debug_func", handler::debug_func);
+    server.set_http_handler<GET>("/api/db/t_user_info_op", handler::t_user_info_op);
 
 }
 
 int main()
 {
-    // const char *port = std::getenv("PORT");
-    // if (port == nullptr)
-    // {
-    //     port = "8080";
-    // }
+    const char *port = std::getenv("PORT");
+    if (port == nullptr)
+    {
+        port = "8080";
+    }
 
-    // SPDLOG_INFO("PORT={}", port);
-    // std::size_t max_thread_num = std::thread::hardware_concurrency();
-    // http_server server(max_thread_num);
-    // server.listen("0.0.0.0", port);
-    // server.set_not_found_handler(handler::not_found_page);
+    SPDLOG_INFO("PORT={}", port);
+    std::size_t max_thread_num = std::thread::hardware_concurrency();
+    http_server server(max_thread_num);
+    server.listen("0.0.0.0", port);
+    server.set_not_found_handler(handler::not_found_page);
 
-    // bind_page(server);
-    // bind_api(server);
+    bind_page(server);
+    bind_api(server);
 
-    // db::database_init();
+    db::database_init();
 
 
-    // server.run();    
+    server.run();    
 
     return 0;
 }
