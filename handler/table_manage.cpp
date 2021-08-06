eb99083a6bba3d9d6f52a42bf9aa9ba37d2237fa
sunbo.kie <sunbo.kie@bytedance.com>
2021-08-06 23:35:55 +0800

    add

--- handler/table_manage.cpp
+++ handler/table_manage.cpp
@@ -47,9 +47,11 @@ namespace handler
         }
 
         auto op_type = request["op_type"].get<std::string>();
-        if(op_type == "insert"){
+        if (op_type == "insert")
+        {
             //Get the parameter
-            if(!utils::all_string(request,{"user_name","password"})){
+            if (!utils::all_string(request, {"user_name", "password"}))
+            {
                 SPDLOG_INFO("user_name or password is not string");
                 res.set_status_and_content(status_type::ok, utils::resp(10001, "user_name or password is not string"), req_content_type::json);
                 return;
@@ -57,42 +59,71 @@ namespace handler
 
             //Check if the name has been occupied
             bool exist = false;
-            std::tie(std::ignore, exist) = db::t_user_info::fetch_first({{"user_name",request["user_name"].get<std::string>()}});
-            if(exist){
+            std::tie(std::ignore, exist) = db::t_user_info::fetch_first({{"user_name", request["user_name"].get<std::string>()}});
+            if (exist)
+            {
                 SPDLOG_INFO("user name '{}' has been registered", request["user_name"].get<std::string>());
                 res.set_status_and_content(status_type::ok, utils::resp(10001, "user name has been occupied"), req_content_type::json);
                 return;
             }
 
             //Insert
-            auto result = db::t_user_info::insert({
-                {"user_name",request["user_name"].get<std::string>()},
-                {"password", request["password"].get<std::string>()},
-                {"create_time", utils::now()}});
-            if(result.affected_rows()==0){
+            auto result = db::t_user_info::insert({{"user_name", request["user_name"].get<std::string>()},
+                                                   {"password", request["password"].get<std::string>()},
+                                                   {"create_time", utils::now()}});
+            if (result.affected_rows() == 0)
+            {
                 SPDLOG_INFO("insert failed");
-                res.set_status_and_content(status_type::ok, utils::resp(10001,"insert failed"), req_content_type::json);
+                res.set_status_and_content(status_type::ok, utils::resp(10001, "insert failed"), req_content_type::json);
                 return;
             }
-        }else if(op_type == "remove"){
+        }
+        else if (op_type == "remove")
+        {
             //Get the parameter
-            if(utils::all_string(request,{"user_name"})){
+            if (utils::all_string(request, {"user_name"}))
+            {
                 SPDLOG_INFO("user_name is missing");
-                res.set_status_and_content(status_type::ok, utils::resp(10001,"user_name is missing"), req_content_type::json);
+                res.set_status_and_content(status_type::ok, utils::resp(10001, "user_name is missing"), req_content_type::json);
                 return;
             }
             auto user_name = request["user_name"].get<std::string>();
 
             //Remove the user_name
-            auto result = db::t_user_info::remove({{"user_name",user_name}}, 1);
-            if(result.affected_rows() == 0){
+            auto result = db::t_user_info::remove({{"user_name", user_name}}, 1);
+            if (result.affected_rows() == 0)
+            {
                 SPDLOG_INFO("failed to remove");
-                res.set_status_and_content(status_type::ok, utils::resp(10001,"failed to remove"),req_content_type::json);
+                res.set_status_and_content(status_type::ok, utils::resp(10001, "failed to remove"), req_content_type::json);
+                return;
+            }
+        }else if(op_type=="fetch"){
+            //Get the parameter
+            if(utils::all_string(request, {"user_name"})){
+                SPDLOG_INFO("user_name is missing");
+                res.set_status_and_content(status_type::ok, utils::resp(10001, "user_name is missing"));
                 return;
             }
 
+            //Get the result
+            auto user_name = request["user_name"].get<std::string>();
+            auto [user_info, exist] = db::t_user_info::fetch_first({{"user_name", user_name}});
+            if(!exist){
+                SPDLOG_INFO("user {} does not exist", user_name);
+                res.set_status_and_content(status_type::ok, utils::resp(10001, fmt::format("user {} does not exist", user_name)));
+                return;
+            }
+            nlohmann::json result;
+            result["user_name"] = user_info.user_name;
+            result["create_time"] = user_info.create_time;
+            result["last_login"] = user_info.last_login;
+            result["email"] = user_info.email;
+            result["profile"] = user_info.profile;
+            
+            res.set_status_and_content(status_type::ok, utils::resp(0,"",result), req_content_type::json);
+            return;
         }
-        res.set_status_and_content(status_type::ok, utils::resp(), req_content_type::json);
+        res.set_status_and_content(status_type::ok, utils::resp(10001, "op_type is not supported"), req_content_type::json);
     }
 
 } // namespace handler
