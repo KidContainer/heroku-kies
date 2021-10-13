d7c0bec33d86946c54274c3c8a3a365751aac833
sunbo.kie <sunbo.kie@bytedance.com>
2021-10-13 20:19:45 +0800

    embrace exception

--- utils/cinatra.hpp
+++ utils/cinatra.hpp
@@ -9,31 +9,22 @@ namespace utils
     template <typename T>
     std::tuple<T, std::string_view, bool> parse_request(const cinatra::request &req)
     {
-        auto log_id = utils::req_id(req);
-        SPDLOG_INFO("log_id={}, content_type={}, true_content_type={}", log_id, req.get_content_type(), req.get_header_value("Content-Type"));
-        //content type check
-        if (req.get_content_type() != cinatra::content_type::string)
-        {
-            SPDLOG_ERROR("log_id={}, request type is not string, it's {}", log_id, req.get_content_type());
-            return {T{}, log_id, false};
-        }
-
         try
         {
-
-            auto j = nlohmann::json::parse(req.body(), nullptr, false);
-            if (j.is_discarded())
+            auto log_id = utils::req_id(req);
+            SPDLOG_INFO("log_id={}, content_type={}, true_content_type={}", log_id, req.get_content_type(), req.get_header_value("Content-Type"));
+            //content type check
+            if (req.get_content_type() != cinatra::content_type::string)
             {
-                SPDLOG_ERROR("log_id={}, message=fail to parse json, body={}", req.body());
+                SPDLOG_ERROR("log_id={}, request type is not string, it's {}", log_id, req.get_content_type());
                 return {T{}, log_id, false};
             }
-            SPDLOG_INFO("log_id={}", log_id);
 
-            return {T{j}, log_id, true};
+            return {T{nlohmann::json::parse(req.body(), nullptr, false)}, log_id, true};
         }
         catch (std::exception &e)
         {
-            SPDLOG_ERROR("log_id={}, request.body()={}, err={}", log_id,req.body(), e.what());
+            SPDLOG_ERROR("log_id={}, request.body()={}, err={}", log_id, req.body(), e.what());
             return {T{}, log_id, false};
         }
     }
